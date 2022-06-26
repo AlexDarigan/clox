ad55c6ed847946042af931c6fae99de85ab6af7c
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2022-06-26 20:43:43 +0100

    26 Complete

--- src/chunk.c
+++ src/chunk.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "chunk.h"
 #include "memory.h"
+#include "vm.h"
 
 void initChunk(Chunk* chunk) {
 	chunk->count = 0;
@@ -11,7 +12,9 @@ void initChunk(Chunk* chunk) {
 }
 
 int addConstant(Chunk* chunk, Value value) {
+	push(value);
 	writeValueArray(&chunk->constants, value);
+	pop();
 	return chunk->constants.count - 1;
 }
 
